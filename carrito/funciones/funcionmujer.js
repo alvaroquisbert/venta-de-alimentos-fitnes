7c44d806fefab5afdf8e33ce190104da1d1fa93b
alvaro <alvaroquisberta70@gmail.com>
2020-10-25 14:44:50 -0400

    carrito

--- carrito/funciones/funcionmujer.js
+++ carrito/funciones/funcionmujer.js
@@ -1,77 +1,77 @@
 {
-    // Variables
+    
     let baseDeDatos = [
         {
             id:1,
-            nombre: 'quema grasa',
-            precio: 1,
-            imagen: src="imagen/1.jpg" 
+            nombre: 'para perder peso',
+            precio: 100,
+            imagen: src="imgmujeres/1.jpg"  
         },
         {
             id:2,
-            nombre: 'centrum',
-            precio: 2,
-            imagen: src="imagen/2.jpg" 
+            nombre: 'magensio',
+            precio: 40,
+            imagen: src="imgmujeres/2.jpg"  
         },
         {
             id:3,
-            nombre: 'energia',
-            precio: 34,
-            imagen: src="imagen/3.jpg" 
+            nombre: 'HBM',
+            precio: 340,
+            imagen: src="imgmujeres/3.jpg"  
         },
         {
             id:4,
-            nombre: 'Reskyle Shake',
-            precio: 4,
-            imagen: src="imagen/4.jpg" 
+            nombre: 'restyle shake',
+            precio: 50,
+            imagen: src="imgmujeres/4.jpg" 
         },
         {
             id:5,
-            nombre: 'vitamina',
-            precio: 5,
-            imagen: src="imagen/5.jpg" 
+            nombre: 'quema grasa',
+            precio: 90,
+            imagen: src="imgmujeres/5.jpg"
         },
         {
             id:6,
-            nombre: 'Nutricion pro hmb',
-            precio:10,
-            imagen: src="imagen/6.jpg" 
+            nombre: 'suplementos fitness',
+            precio:100,
+            imagen: src="imgmujeres/6.jpg" 
         },
         {
             id:7,
-            nombre: 'Patata',
-            precio: 70,
-            imagen: src="imagen/7.jpg" 
+            nombre: 'whilshak',
+            precio: 78,
+            imagen: src="imgmujeres/7.jpg"
         },
         {
             id:8,
-            nombre: 'Patata',
-            precio: 8,
-            imagen: src="imagen/8.jpg" 
+            nombre: 'hmb300',
+            precio: 150,
+            imagen: src="imgmujeres/8.jpg"
         },
         {
             id:9,
-            nombre: 'Patata',
-            precio: 20,
-            imagen: src="imagen/9.jpg" 
+            nombre: 'cartne quema grasa',
+            precio: 230,
+            imagen: src="imgmujeres/9.jpg" 
         },
         {
             id:10,
-            nombre: 'Patata',
-            precio: 1,
-            imagen: src="imagen/10.jpg" 
+            nombre: 'trenodol',
+            precio: 90,
+            imagen: src="imgmujeres/10.jpg"
         },
         {
             id:11,
-            nombre: 'Patata',
-            precio: 20,
-            imagen: src="imagen/11.jpg" 
+            nombre: 'Creatine',
+            precio: 156,
+            imagen: src="imgmujeres/11.jpg"
         },
         {
             id:12,
-            nombre: 'Patata',
-            precio: 20,
-            imagen: src="imagen/12.jpg" 
+            nombre: 'Proteina mas vitamina',
+            precio: 140,
+            imagen: src="imgmujeres/12.jpg" 
         },
      
 
@@ -83,34 +83,34 @@
     let $total = document.querySelector('#total');
     let $botonVaciar = document.querySelector('#boton-vaciar');
 
-    // Funciones
+   
     function renderItems() {
         for(let info of baseDeDatos) {
-            // Estructura
+            
             let miNodo = document.createElement('div');
             miNodo.classList.add('card', 'col-sm-4');
-            // Body
+         
             let miNodoCardBody = document.createElement('div');
             miNodoCardBody.classList.add('card-body');
-            // Titulo
+          
             let miNodoTitle = document.createElement('h5');
             miNodoTitle.classList.add('card-title');
             miNodoTitle.textContent = info['nombre'];
-            // Imagen
+          
             let miNodoImagen = document.createElement('img');
             miNodoImagen.classList.add('img-fluid');
             miNodoImagen.setAttribute('src', info['imagen']);
-            // Precio
+           
             let miNodoPrecio = document.createElement('p');
             miNodoPrecio.classList.add('card-text');
-            miNodoPrecio.textContent = info['precio'] + '€';
-            // Boton 
+            miNodoPrecio.textContent = info['precio'] + 'Bs';
+            
             let miNodoBoton = document.createElement('button');
             miNodoBoton.classList.add('btn', 'btn-primary');
             miNodoBoton.textContent = '+';
             miNodoBoton.setAttribute('marcador', info['id']);
             miNodoBoton.addEventListener('click', anyadirCarrito);
-            // Insertamos
+            
             miNodoCardBody.appendChild(miNodoImagen);
             miNodoCardBody.appendChild(miNodoTitle);
             miNodoCardBody.appendChild(miNodoPrecio);
@@ -121,41 +121,41 @@
     }
 
     function anyadirCarrito () {
-        // Anyadimos el Nodo a nuestro carrito
+       
         carrito.push(this.getAttribute('marcador'))
-        // Calculo el total
+       
         calcularTotal();
-        // Renderizamos el carrito 
+      
         renderizarCarrito();
     }
 
     function renderizarCarrito() {
-        // Vaciamos todo el html
+        
         $carrito.textContent = '';
-        // Quitamos los duplicados
+      
         let carritoSinDuplicados = [...new Set(carrito)];
-        // Generamos los Nodos a partir de carrito
+        
         carritoSinDuplicados.forEach(function (item, indice) {
-            // Obtenemos el item que necesitamos de la variable base de datos
+           
             let miItem = baseDeDatos.filter(function(itemBaseDatos) {
                 return itemBaseDatos['id'] == item;
             });
-            // Cuenta el número de veces que se repite el producto
+           
             let numeroUnidadesItem = carrito.reduce(function (total, itemId) {
                 return itemId === item ? total += 1 : total;
             }, 0);
-            // Creamos el nodo del item del carrito
+            
             let miNodo = document.createElement('li');
             miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
             miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0]['nombre']} - ${miItem[0]['precio']}€`;
-            // Boton de borrar
+       
             let miBoton = document.createElement('button');
             miBoton.classList.add('btn', 'btn-danger', 'mx-5');
             miBoton.textContent = 'X';
             miBoton.style.marginLeft = '1rem';
             miBoton.setAttribute('item', item);
             miBoton.addEventListener('click', borrarItemCarrito);
-            // Mezclamos nodos
+          
             miNodo.appendChild(miBoton);
             $carrito.appendChild(miNodo);
         })
@@ -163,46 +163,55 @@
 
     function borrarItemCarrito() {
         console.log()
-        // Obtenemos el producto ID que hay en el boton pulsado
         let id = this.getAttribute('item');
-        // Borramos todos los productos
-        carrito = carrito.filter(function (carritoId) {
+      carrito = carrito.filter(function (carritoId) {
             return carritoId !== id;
         });
-        // volvemos a renderizar
         renderizarCarrito();
-        // Calculamos de nuevo el precio
         calcularTotal();
     }
 
     function calcularTotal() {
-        // Limpiamos precio anterior
         total = 0;
-        // Recorremos el array del carrito
+      
         for (let item of carrito) {
-            // De cada elemento obtenemos su precio
             let miItem = baseDeDatos.filter(function(itemBaseDatos) {
                 return itemBaseDatos['id'] == item;
             });
             total = total + miItem[0]['precio'];
         }
-        // Formateamos el total para que solo tenga dos decimales
         let totalDosDecimales = total.toFixed(2);
-        // Renderizamos el precio en el HTML
         $total.textContent = totalDosDecimales;
     }
 
     function vaciarCarrito() {
-        // Limpiamos los productos guardados
         carrito = [];
-        // Renderizamos los cambios
         renderizarCarrito();
         calcularTotal();
     }
-
-    // Eventos
     $botonVaciar.addEventListener('click', vaciarCarrito);
-
-    // Inicio
     renderItems();
 } 
+function convertir(){
+    medida=document.getElementById("medida");
+    categoria=document.getElementById("categoria");
+    medida=medida.value;
+    categoria=categoria.value;
+    salida='';
+    switch(categoria){
+        case '1':{
+            salida=(medida*0.14).toFixed(2)+'Dolares';
+        }
+        break;
+        case '2':{
+            salida=(medida*0.13).toFixed(2)+'Euros';
+        }
+        break;
+        case '3':{
+            salida=(medida*0.77).toFixed(2)+'Reales';
+        }
+        break;
+    }
+    transformacion=document.getElementById("resultado");
+    transformacion.innerHTML=salida;
+}
